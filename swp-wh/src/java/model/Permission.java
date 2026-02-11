b2052c96b3637a6f4247319219ffec148d05e818
XuanToan01-hn <xuantoan10022002@gmail.com>
2026-02-11 22:34:22 +0700

    add entity for role permission

--- swp-wh/src/java/model/Permission.java
+++ swp-wh/src/java/model/Permission.java
@@ -9,5 +9,26 @@ package model;
  * @author Asus
  */
 public class Permission {
+    private int id;
+    private String name;
+    private String url;
+    private String description;
+    private PermissionGroup group;
 
+    public Permission() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getUrl() { return url; }
+    public void setUrl(String url) { this.url = url; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public PermissionGroup getGroup() { return group; }
+    public void setGroup(PermissionGroup group) { this.group = group; }
 }
--- swp-wh/src/java/model/PermissionGroup.java
+++ swp-wh/src/java/model/PermissionGroup.java
@@ -9,6 +9,20 @@ package model;
  * @author Asus
  */
 public class PermissionGroup {
+    private int id;
+    private String groupName;
+    private String description;
 
+    public PermissionGroup() {}
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getGroupName() { return groupName; }
+    public void setGroupName(String groupName) { this.groupName = groupName; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
 }
+
 
